7469a1557e9def89130ac98ee928740157e9c68c
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-09-29 16:25:18 +0900

    Fix: Footer 에러 수정

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -111,7 +111,6 @@ export default function Footer() {
         <div className="bg-gray-100">
           <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               <span className="ml-3 text-xl">Parabole</span>
             </a>
             <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
