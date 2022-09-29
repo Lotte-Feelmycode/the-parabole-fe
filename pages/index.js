5bf7e4189eed4aaf63daad2d71fd0c1cddfa26b3
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-09-29 14:59:16 +0900

    Feat: 상품 목록 구현

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import CommerceLayout from '@components/common/CommerceLayout';
 import SiteHead from '@components/common/SiteHead.js';
+import ProductList from '@components/product/ProductList';
 
 export default function Home() {
+  const productListProps = {
+    size: 10,
+    page: 1,
+  };
+
   return (
     <CommerceLayout>
       <SiteHead title={'Home'} />
@@ -9,6 +15,7 @@ export default function Home() {
       <section className="flex min-h-screen flex-col text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <h1>THE PARABOLE</h1>
+          <ProductList {...productListProps} />
         </div>
       </section>
     </CommerceLayout>
