4bfbf2858a8dd06d16ea805a97988bc8fc2519ca
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-09-30 15:19:21 +0900

    Feat: change globals.css to scss

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import CommerceLayout from '@components/common/CommerceLayout';
 import SiteHead from '@components/common/SiteHead.js';
 import ProductList from '@components/product/ProductList';
+import styles from '@styles/Home.module.scss';
 
 export default function Home() {
   const productListProps = {
@@ -14,7 +15,7 @@ export default function Home() {
 
       <section className="flex min-h-screen flex-col text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <h1>THE PARABOLE</h1>
+          <h1 className={styles.section}>THE PARABOLE</h1>
           <ProductList {...productListProps} />
         </div>
       </section>
