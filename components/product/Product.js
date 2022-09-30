1f5848737184a9ae6e794e9a3a27d1b9ac43e5ea
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-09-30 15:30:08 +0900

    Fix: css in js 처리

--- components/product/Product.js
+++ components/product/Product.js
@@ -17,29 +17,36 @@ export default function Product({ product }) {
   };
 
   return (
-    <div className={styles.productInfo}>
-      <a onClick={() => onClick(product.productId || 1, product.productName)}>
-        <div>
-          <img
-            className="productImg"
-            src="https://contents.lotteon.com/itemimage/_v114501/LO/10/00/39/35/55/_1/00/03/93/55/6/LO1000393555_1000393556_1.jpg/dims/optimize/dims/resizemc/360x360"
-            alt="천연 순면 마스크팩 시트 4종 100매 티트리 쉐어버터 로얄제리젤리 알로에"
-          />
-        </div>
-      </a>
-      <div className="product-body">
-        <div className="srchProductInfoColumn">
-          <div className="srchProductUnitTitle">{product.productName}</div>
-        </div>
-        <div className="srchProductInfoColumn">
-          <div className="s-product-price">
-            <strong className="s-product-price-final">
-              <span className="s-product-price-number">000,000</span>원
-            </strong>
+    <>
+      <div className={styles.productInfo}>
+        <a onClick={() => onClick(product.productId || 1, product.productName)}>
+          <div>
+            <img
+              className="productImg"
+              src="https://contents.lotteon.com/itemimage/_v114501/LO/10/00/39/35/55/_1/00/03/93/55/6/LO1000393555_1000393556_1.jpg/dims/optimize/dims/resizemc/360x360"
+              alt="천연 순면 마스크팩 시트 4종 100매 티트리 쉐어버터 로얄제리젤리 알로에"
+            />
           </div>
+        </a>
+        <div className="product-body">
+          <div className="srchProductInfoColumn">
+            <div className="srchProductUnitTitle">{product.productName}</div>
+          </div>
+          <div className="srchProductInfoColumn">
+            <div className="s-product-price">
+              <strong className="s-product-price-final">
+                <span className="s-product-price-number">000,000</span>원
+              </strong>
+            </div>
+          </div>
+          <div className="srchProductInfoColumn"></div>
         </div>
-        <div className="srchProductInfoColumn"></div>
       </div>
-    </div>
+      <style jsx>{`
+        .productImg {
+          width: 300px;
+        }
+      `}</style>
+    </>
   );
 }
