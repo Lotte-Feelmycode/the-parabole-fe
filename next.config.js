3338487ccfa5c2449dae35b72fd43d36484569a7
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-10-04 11:59:07 +0900

    Feat: emotion 설정 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
