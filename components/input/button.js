ce30870a4accbe5a897b7591e952dd2c972b5cb0
IMHYEWON <hyewon1118@gmail.com>
2022-10-05 15:16:22 +0900

    Feat: 공통 버튼 색상 버전 추가

--- components/input/button.js
+++ components/input/button.js
@@ -1,9 +1,26 @@
 import styled from '@emotion/styled';
 
-const PostButton = ({ id, name, buttonText, onClickFunc }) => {
+const PostButton = ({ id, name, buttonText, onClickFunc, version }) => {
+  var fontColor = '#ffffff';
+  var backgroundColor = 'tomato';
+
+  if (version == 'light') {
+    fontColor = 'black';
+    backgroundColor = '#f9f9f9';
+  } else if (version == 'dark') {
+    fontColor = '#ffffff';
+    backgroundColor = '#000000';
+  }
+
   return (
-    <div className="posting-button-container">
-      <Button id={id} name={name} onClick={onClickFunc}>
+    <div>
+      <Button
+        id={id}
+        name={name}
+        onClick={onClickFunc}
+        fontColor={fontColor}
+        backgroundColor={backgroundColor}
+      >
         {buttonText}
       </Button>
     </div>
@@ -11,9 +28,9 @@ const PostButton = ({ id, name, buttonText, onClickFunc }) => {
 };
 
 const Button = styled.button`
-  background-color: tomato;
-  color: white;
-  focust: outline-none;
+  background-color: ${(props) => props.backgroundColor};
+  color: ${(props) => props.fontColor};
+  focus: outline-none;
   border-radius: 0.25rem;
   cursor: pointer;
   padding: 0.5em 1.5em 0.5em 1.5em;
