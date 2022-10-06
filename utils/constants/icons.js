96183b91c2b41b339f4d3da4d10b1517d5810012
IMHYEWON <hyewon1118@gmail.com>
2022-10-06 21:32:03 +0900

    Design: 검색돋보기 아이콘 svg파일 추가, icon상수로 선언

---
+++ utils/constants/icons.js
@@ -0,0 +1,6 @@
+export const ICON_SEARCH_MAGNIFY =
+  require('@assets/search-magnifiying-glass.svg').default.src;
+
+export const LOGO_PARABOLE = require('@public/parabole.svg').default.src;
+export const LOGO_PARABOLE_PINK = require('@public/parabole_pink.svg').default
+  .src;
