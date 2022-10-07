dea687bcc59e41f18a776d89ce34b869cca678f3
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-10-07 14:35:34 +0900

    Fix: 테마 컬러 변경

--- utils/constants/themeColor.js
+++ utils/constants/themeColor.js
@@ -1,10 +1,10 @@
-export const MainColor1 = '#145C9E';
+export const MainColor1 = '#0084FF';
 export const ThemeColor1_1 = '#42BFDD';
 export const ThemeColor1_2 = '#BBE6E4';
 
-export const MainColor2 = '#E0B0D5';
-export const ThemeColor2_1 = '#E3BAC6';
-export const ThemeColor2_2 = '#faf7c8';
+export const MainColor2 = '#FC4D99';
+export const ThemeColor2_1 = '#f283b6';
+export const ThemeColor2_2 = '#eed2cc';
 
 export const ThemeWhite = '#F0F6F6';
 export const ThemeGray = '#D6D6D6';
