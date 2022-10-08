08214e8a5dbc30e543733bed02aae36e15bde955
IMHYEWON <hyewon1118@gmail.com>
2022-10-08 16:34:02 +0900

    Fix: Seller 메인 레이아웃 및 url 수정

--- pages/seller/main.js
+++ pages/seller/main.js
@@ -1,16 +1,25 @@
 import SellerLayout from '@components/seller/SellerLayout';
-import SiteHead from '@components/common/SiteHead.js';
+import Link from 'next/link';
+import Heading from '@components/input/Heading';
 
 export default function Home() {
   return (
     <SellerLayout>
-      <SiteHead title={'Seller Office'} />
-
-      <section className="flex min-h-screen flex-col text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <h1>THE PARABOLE SELLER OFFICE</h1>
-        </div>
-      </section>
+      <div>
+        <Heading title="🎁 이벤트" type="h1" />
+        <ul>
+          <li>
+            <Link href="event/newevent">
+              <a>👉 이벤트 등록</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="event/list">
+              <a>👉 이벤트 목록</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </SellerLayout>
   );
 }
