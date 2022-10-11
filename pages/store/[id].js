3fa014302e6414bcede081e48c364f378f48fcbf
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-10-11 10:42:18 +0900

    Fix: 판매자 페이지 import 오류 수정

--- pages/store/[id].js
+++ pages/store/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-import { ProductList } from '@components/product/ProductList';
+import ProductList from '@components/product/ProductList';
 
 export default function store() {
   const router = useRouter();
