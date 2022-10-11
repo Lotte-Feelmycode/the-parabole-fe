c98c1b5eb593a71839b65d8e9b9d0744e5e09c03
IMHYEWON <hyewon1118@gmail.com>
2022-10-11 19:28:26 +0900

    Fix: 셀러 헤더 url수정

--- components/seller/SellerHeader.js
+++ components/seller/SellerHeader.js
@@ -23,7 +23,7 @@ export default function SellerHeader() {
             <Link href="/seller/main">
               <a className="mr-5 hover:text-gray-900">홈</a>
             </Link>
-            <Link href="/seller/event">
+            <Link href="/seller/event/list">
               <a className="mr-5 hover:text-gray-900">이벤트</a>
             </Link>
             <Link href="/">
