881f05476e8ecdf3512cf56621839b5f50ca95f2
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-10-11 10:31:33 +0900

    Fix: 메인 Header 수정(버튼제거, 링크 오타 수정)

--- components/seller/SellerHeader.js
+++ components/seller/SellerHeader.js
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 
 export default function SellerHeader() {
+  const linkSellerHome = '/seller/main';
+
   return (
     <>
       <header className="text-gray-700 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a
-            href="/sellerOffice"
+            href={linkSellerHome}
             className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
           >
             <img src="/parabole_pink.svg" className="sellerlogo" />
@@ -20,7 +22,7 @@ export default function SellerHeader() {
             <span className="ml-3 text-xl">The Parabole Seller</span>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href="/seller/main">
+            <Link href={linkSellerHome}>
               <a className="mr-5 hover:text-gray-900">홈</a>
             </Link>
             <Link href="/">
@@ -33,20 +35,6 @@ export default function SellerHeader() {
               <a className="mr-5 hover:text-gray-900">회원가입</a>
             </Link>
           </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
         </div>
       </header>
     </>
