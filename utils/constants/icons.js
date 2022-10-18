def5f80656cd82c1db25cc7f536bd2c950faaeb8
IMHYEWON <hyewon1118@gmail.com>
2022-10-18 21:02:56 +0900

    Design: 등록, 목록, 체크 아이콘 추가

--- utils/constants/icons.js
+++ utils/constants/icons.js
@@ -4,6 +4,12 @@ export const ICON_CART_BLUE = require('@assets/cart-blue.svg').default.src;
 export const ICON_CART_TRANS = require('@assets/cart-transparent.svg').default
   .src;
 export const ICON_CART_BLACK = require('@assets/cart-black.svg').default.src;
+export const ICON_CART_BLACK_ITEM = require('@assets/cart-black-item.svg')
+  .default.src;
+
+export const ICON_POST = require('@assets/icon-post.svg').default.src;
+export const ICON_LIST = require('@assets/icon-list.svg').default.src;
+export const ICON_CHECK = require('@assets/icon-check.svg').default.src;
 
 export const ICON_SHOP = require('@assets/icon-shop.svg').default.src;
 export const ICON_PERCENT = require('@assets/icon-percent.svg').default.src;
