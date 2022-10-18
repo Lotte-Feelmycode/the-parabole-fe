d6ea069e5a15b4f8a8d84bf4ab1c39e05480a2db
IMHYEWON <hyewon1118@gmail.com>
2022-10-18 10:13:35 +0900

    Chore: AirBnB스타일 적용안된부분 인덴트 수정

--- utils/constants/icons.js
+++ utils/constants/icons.js
@@ -9,9 +9,12 @@ export const ICON_PERCENT = require('@assets/icon-percent.svg').default.src;
 export const ICON_TRUCK = require('@assets/icon-truck.svg').default.src;
 export const ICON_SEARCH = require('@assets/icon-search.svg').default.src;
 export const ICON_OPEN = require('@assets/icon-open.svg').default.src;
-export const ICON_HEART_BAG = require('@assets/icon-heart-shopping-bag.svg').default.src;
-export const ICON_ONLINE_SHOPPING = require('@assets/icon-online-shopping.svg').default.src;
-export const ICON_ONLINE_SHOPPING_2 = require('@assets/icon-online-shopping-2.svg').default.src;
+export const ICON_HEART_BAG = require('@assets/icon-heart-shopping-bag.svg')
+  .default.src;
+export const ICON_ONLINE_SHOPPING = require('@assets/icon-online-shopping.svg')
+  .default.src;
+export const ICON_ONLINE_SHOPPING_2 =
+  require('@assets/icon-online-shopping-2.svg').default.src;
 
 export const LOGO_PARABOLE = require('@public/parabole.svg').default.src;
 export const LOGO_PARABOLE_PINK = require('@public/parabole_pink.svg').default
