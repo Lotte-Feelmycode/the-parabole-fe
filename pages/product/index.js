a0be23893866ec37e11015a8eadbf4170e97465f
IMHYEWON <hyewon1118@gmail.com>
2022-10-18 18:43:11 +0900

    Feat: 상품 페이지 생성

---
+++ pages/product/index.js
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+
+import CommerceLayout from '@components/common/CommerceLayout';
+import SiteHead from '@components/common/SiteHead.js';
+import { useRouter } from 'next/router';
+import ProductList from '@components/product/ProductList';
+import CustomSwiper from '@components/common/CustomSwiper';
+export default function Products({}) {
+  const router = useRouter();
+
+  const inputSearchValue = router.query.searchValue;
+  const productListProps = {
+    size: 6,
+    page: 0,
+    productName: inputSearchValue,
+  };
+
+  return (
+    <>
+      <CommerceLayout>
+        <SiteHead title="Home" />
+        <div className="container px-5 py-12 mx-auto">
+          <CustomSwiper></CustomSwiper>
+          <Row>
+            <ProductList {...productListProps} />
+          </Row>
+        </div>
+      </CommerceLayout>
+    </>
+  );
+}
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  margin-right: -10px;
+  margin-left: -10px;
+  align-items: stretch;
+  margin-bottom: 40px;
+`;
