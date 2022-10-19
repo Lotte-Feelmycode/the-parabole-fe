236c807472e2dac728447c275ab33ba5a919d0b6
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-10-19 12:21:14 +0900

    Fix: 주문 페이지 변경 및 백엔드 api 연결

--- pages/user/order/index.js
+++ pages/user/order/index.js
@@ -3,53 +3,120 @@ import CommerceLayout from '@components/common/CommerceLayout';
 import styled from '@emotion/styled';
 import { createContext, useEffect, useState } from 'react';
 import Input from '@components/input/Input';
-import * as btn from '@components/input/Button';
 import ProductList from '@components/order/ProductList';
 import { numberToMonetary } from '@utils/moneyUtil';
 import PayList from '@components/order/PayList';
-import { GET_DATA, POST_DATA } from '@apis/defaultApi';
+import { POST, GET } from '@apis/defaultApi';
+import { ThemeGray2 } from '@utils/constants/themeColor';
+import { Blue } from '@components/input/Button';
+import { useRouter } from 'next/router';
+import { isEmpty } from '@utils/functions';
 
 export const AppContext = createContext();
 
 export default function OrderAndPayment() {
   const getUserId = 3;
+  const router = useRouter();
 
   const [userId, setUserId] = useState(getUserId);
-  // product를 받아서 값을 넣어야 하지만 지금은 그냥 들어가 있는 테스트 값
   const [product, setProduct] = useState([]);
+  const [orderInfoResponseDto, setOrderInfoResponseDto] = useState([]);
+  const [payState, setPayState] = useState(-1);
 
-  function getSum() {
-    let sum = 0;
-    for (var i = 0; i < product.length; i++) {
-      sum = sum + product[i].productPrice;
-    }
-    return sum;
-  }
+  // TODO 초기값에 user정보 넣기
+  const [receiverName, setReceiverName] = useState('');
+  const [receivePhone, setReceivePhone] = useState('');
+  const [receiveAddress, setReceiveAddress] = useState('');
+  const [receiveMemo, setReceiveMemo] = useState('');
 
   useEffect(() => {
-    console.log(userId);
-    GET_DATA(`/orderinfo`, { userId }).then((res) => {
+    GET(`/orderinfo`, { userId }).then((res) => {
       console.log(res);
-      setProduct(res);
+      if (res && res.data) {
+        setOrderInfoResponseDto(res.data);
+      } else {
+        alert('주문 정보가 없습니다.');
+        router.push('/');
+      }
     });
   }, []);
 
-  const params = {
-    userId: userId,
-    orderState: '주문 확정',
-  };
+  useEffect(() => {
+    if (orderInfoResponseDto && orderInfoResponseDto.length > 0) {
+      orderInfoResponseDto.forEach((dto) => {
+        setProduct([
+          ...product,
+          {
+            orderInfoId: dto.id,
+            orderinfoState: dto.state,
+            userId: dto.userId,
+            userEmail: dto.userEmail,
+            productId: dto.productId,
+            productName: dto.productName,
+            productCnt: dto.productCnt,
+            productRemain: dto.productRemain,
+            productPrice: dto.productPrice,
+            productDiscountPrice: dto.productDiscountPrice,
+            productThumbnailImg: dto.productThumbnailImg,
+          },
+        ]);
+      });
+    }
+  }, [orderInfoResponseDto]);
 
-  function orderProduct() {
-    POST_DATA(`/order`, params).then((res) => {
-      setProduct(res);
+  function goToPayment() {
+    const orderState = '주문 확정';
+
+    if (payState === -1) {
+      alert('결제수단을 선택해서 결제를 진행해주세요');
+      return;
+    }
+    if (isEmpty(receiverName)) {
+      alert('수령인을 입력해주세요');
+      return;
+    }
+    if (isEmpty(receivePhone)) {
+      alert('수령인 전화번호를 입력해주세요');
+      return;
+    }
+    if (isEmpty(receiveAddress)) {
+      alert('수령인 주소를 입력해주세요');
+      return;
+    }
+
+    POST(`/order`, { userId, orderState }).then((res) => {
+      console.log(res);
+      if (res) {
+        if (res.success) {
+          router.push('/user/mypage');
+        } else {
+          alert('주문에 실패했습니다. 다시시도해주세요');
+        }
+      }
     });
+  }
+
+  function getProductTotalPrice(products) {
+    if (!products) return;
+    let total = 0;
+    products.map((products) => {
+      total += products.productPrice;
+    });
+    return total;
+  }
+
+  function sameAsUser(flag) {
+    if (flag) {
+      //TODO 사용자 정보 불러오기
+      setReceiverName('userId.userName');
+      setReceivePhone('userId.userPhone');
+    }
   }
 
   const input = {
     width: '14rem',
     borderRadius: '0.2rem',
-    backgroundColor: '#e1e1e1',
-    border: 'none',
+    border: 'solid 1px ' + ThemeGray2,
     fontSize: '1rem',
     margin: '10px auto',
   };
@@ -57,23 +124,33 @@ export default function OrderAndPayment() {
   return (
     <Div>
       <CommerceLayout>
-        <SiteHead title="Order / Payment" />
-        <section className="flex min-h-screen flex-col text-gray-600 body-font">
-          <div className="container px-5 py-24 mx-auto">
-            <H1>주문 목록</H1>
-            <H2>주문자 정보</H2>
+        <SiteHead title="Order/Payment" />
+        <OrderContainer className="order-container">
+          <OrderSection className="order-section">
+            <H1>주문/결제</H1>
+            <H2>주문자</H2>
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
               <tbody>
                 <tr>
                   <TdTitle>주문자</TdTitle>
                   <Td>
-                    <Input css={input} />
+                    <Input
+                      // TODO : user 정보 넣기
+                      css={input}
+                      attr={{ readOnly: true }}
+                      value={'userId.name'}
+                    />
                   </Td>
                 </tr>
                 <tr>
                   <TdTitle>휴대전화</TdTitle>
                   <Td>
-                    <Input css={input} />
+                    <Input
+                      // TODO : user 정보 넣기
+                      css={input}
+                      attr={{ readOnly: true }}
+                      value={'userId.phone'}
+                    />
                   </Td>
                 </tr>
               </tbody>
@@ -81,33 +158,62 @@ export default function OrderAndPayment() {
             <br />
             <hr />
             <br />
-            <H2>수령자 정보</H2>
-            <div>(체크박스가 들어갈 것(주문자 정보와 동일)</div>
-            {/* <btn.SmallLineWhite buttonText={'주문자 정보와 동일'} /> */}
+            <H2>수령자</H2>
+            <SameAsUserSection>
+              <input
+                type="checkBox"
+                onChange={(event) => {
+                  sameAsUser(event.target.checked);
+                }}
+              />
+              <span> 주문자 정보와 동일</span>
+            </SameAsUserSection>
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
               <tbody>
                 <tr>
                   <TdTitle>수령인</TdTitle>
                   <Td>
-                    <Input css={input} />
+                    <Input
+                      type="text"
+                      css={input}
+                      value={receiverName}
+                      onChange={(event) => {
+                        setReceiverName(event.target.value);
+                      }}
+                    />
                   </Td>
                 </tr>
                 <tr>
                   <TdTitle>휴대전화</TdTitle>
                   <Td>
-                    <Input css={input} />
+                    <Input
+                      type="text"
+                      css={input}
+                      value={receivePhone}
+                      onChange={(event) => {
+                        setReceivePhone(event.target.value);
+                      }}
+                    />
                   </Td>
                 </tr>
                 <tr>
                   <TdTitle>배송지 주소</TdTitle>
                   <Td>
-                    <Input css={input} />
+                    <Input
+                      css={input}
+                      value={receiveAddress}
+                      onChange={setReceiveAddress}
+                    />
                   </Td>
                 </tr>
                 <tr>
                   <TdTitle>배송 메모</TdTitle>
                   <Td>
-                    <Input css={input} />
+                    <Input
+                      css={input}
+                      value={receiveMemo}
+                      onChange={setReceiveMemo}
+                    />
                   </Td>
                 </tr>
               </tbody>
@@ -120,30 +226,142 @@ export default function OrderAndPayment() {
             <br />
             <hr />
             <br />
-            {/* <div> */}
-            {/* <H2>쿠폰 / 할인</H2> */}
-            {/* <>총 금액</>&nbsp;&nbsp;&nbsp; */}
-            {/* <Total>{numberToMonetary(getOrderTotal(product))}원</Total> */}
-            {/* </div> */}
-            {/* <br /> */}
-            {/* <hr /> */}
-            {/* <br /> */}
             <div>
-              <H2>결제방식</H2>
-              <PayList />
+              <H2>쿠폰 / 할인</H2>
+              <>총 금액 </>
+              <Total>{numberToMonetary(getProductTotalPrice(product))}원</Total>
             </div>
             <br />
+            <hr />
             <br />
-            <btn.Blue
-              buttonText={numberToMonetary(getSum()) + '원 결제하기'}
-              onClickFunc={orderProduct}
-            />
-          </div>
-        </section>
+            <div>
+              <H2>결제방식</H2>
+              <PayList index={payState} setIndex={setPayState} />
+            </div>
+          </OrderSection>
+          <SideBarSection className="sidebar-section">
+            <StickyContainer>
+              <PayResultSection
+                className="pay-result-section"
+                borderColor={ThemeGray2}
+              >
+                <h3>결제금액</h3>
+                <TotalProductPriceSection>
+                  <PayIndex>
+                    <PayLable>총 상품금액</PayLable>
+                    <PayPrice marginLeft="auto">
+                      {numberToMonetary(getProductTotalPrice(product)) || 0}원
+                    </PayPrice>
+                  </PayIndex>
+                  <PayIndex>
+                    <PayLable>할인금액</PayLable>
+                    <PayPrice marginLeft="auto">{0}원</PayPrice>
+                  </PayIndex>
+                  <PayIndex>
+                    <PayLable>배송비</PayLable>
+                    <PayPrice marginLeft="auto">{0}원</PayPrice>
+                  </PayIndex>
+                  <PayLargeIndex borderColor={ThemeGray2}>
+                    <PayLargeLable>최종 결제 금액</PayLargeLable>
+                    <PayLargePrice>
+                      {numberToMonetary(getProductTotalPrice(product)) || 0}원
+                    </PayLargePrice>
+                  </PayLargeIndex>
+                </TotalProductPriceSection>
+              </PayResultSection>
+              <Blue
+                buttonText={
+                  numberToMonetary(getProductTotalPrice(product)) +
+                  '원 결제하기'
+                }
+                onClickFunc={goToPayment}
+                css={{ width: '100%' }}
+              />
+            </StickyContainer>
+          </SideBarSection>
+        </OrderContainer>
+        <br />
+        <hr />
       </CommerceLayout>
     </Div>
   );
 }
+
+const OrderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  flex-wrap: wrap;
+`;
+
+const OrderSection = styled.div`
+  padding-left: 0px;
+  max-width: 100%;
+  flex: 1 0 0px;
+`;
+
+const SideBarSection = styled.div`
+  padding-top: 60px;
+  margin-left: 40px;
+  flex: 0 1 0px;
+  max-width: 100%;
+  min-width: 300px;
+`;
+
+const StickyContainer = styled.div`
+  position: sticky;
+  top: 81px;
+  padding-top: 40px;
+`;
+
+const PayResultSection = styled.div`
+  border: solid 1px ${(props) => props.borderColor};
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  margin-bottom: 10px;
+  padding: 20px;
+`;
+
+const PayIndex = styled.div`
+  display: inline-flex;
+  margin-top: 5px;
+`;
+
+const PayLable = styled.h4`
+  font-size: 1rem;
+`;
+
+const PayPrice = styled.span`
+  margin-left: auto;
+  font-size: 1rem;
+`;
+
+const PayLargeIndex = styled.div`
+  border-top: solid 1px ${(props) => props.borderColor};
+  display: inline-flex;
+  margin-top: 20px;
+  padding-top: 19px;
+`;
+
+const PayLargeLable = styled.h4`
+  font-size: 1.2rem;
+`;
+
+const PayLargePrice = styled.span`
+  margin-left: auto;
+  font-size: 1.2rem;
+`;
+
+const TotalProductPriceSection = styled.div`
+  margin: 10px 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SameAsUserSection = styled.div`
+  padding: 5px 0;
+  font-size: 1rem;
+`;
 
 const Div = styled.div`
   font-size: 1.3rem;
@@ -154,7 +372,7 @@ const Total = styled.span`
 `;
 
 const Td = styled.td`
-  text-align: center;
+  text-align: right;
   width: 10rem;
 `;
 
@@ -170,4 +388,5 @@ const H1 = styled.h1`
 
 const H2 = styled.p`
   font-size: 2rem;
+  margin-bottom: 20px;
 `;
