1df8416c2ea439698d3c40aa6c7c57c22ace746b
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-10-22 18:22:46 +0900

    Feat: 마이페이지 쿠폰 리스트 추가

--- pages/user/mypage/index.js
+++ pages/user/mypage/index.js
@@ -3,36 +3,43 @@ import { useState } from 'react';
 import UserOrderList from '@components/order/UserOrderList';
 import EventApplyList from '@components/user/EventApplyList';
 import MyProfile from '@components/user/MyProfile';
+import SiteHead from '@components/common/SiteHead.js';
+import styled from '@emotion/styled';
+import { ThemeBlueWhite } from '@utils/constants/themeColor';
+import UserCouponList from '@components/coupon/UserCouponList';
 
 export default function () {
   // TODO: userId 집어넣기
   const userId = 3;
 
   const [nowState, setNowState] = useState(0);
-  const mypageStateList = ['나의 주문', '나의 이벤트', '나의 프로필'];
+  const mypageStateList = [
+    '나의 주문',
+    '나의 이벤트',
+    '나의 쿠폰',
+    '나의 프로필',
+  ];
 
   function showMypageMainComp(input) {
     if (input === 0) {
-      return (
-        <div>
-          <UserOrderList userId={userId} />
-        </div>
-      );
+      return <UserOrderList userId={userId} />;
     } else if (input === 1) {
-      return <EventApplyList />;
+      return <EventApplyList userId={userId} />;
     } else if (input === 2) {
-      return (
-        <div>
-          <MyProfile userId={userId}></MyProfile>
-        </div>
-      );
+      return <UserCouponList userId={userId} />;
+    } else if (input === 3) {
+      return <MyProfile userId={userId} />;
     }
   }
 
   return (
     <CommerceLayout>
-      <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center border-b bg-gray-100">
-        <ul className="list-none text-center whitespace-nowrap">
+      <SiteHead title="My page" />
+      <NavSection
+        color={ThemeBlueWhite}
+        className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center border-b"
+      >
+        <ul className="contents list-none text-center whitespace-nowrap">
           {mypageStateList.map((state, index) => (
             <li className="float-left">
               <a
@@ -40,7 +47,7 @@ export default function () {
                 onClick={() => {
                   setNowState(index);
                 }}
-                className="flex title-font text-lg font-semibold items-center p-5 text-gray-900 mb-4 md:mb-0 cursor-pointer"
+                className="flex title-font text-lg font-semibold items-center p-5 text-gray-900  cursor-pointer"
               >
                 {nowState === index && (
                   <span className="ml-3 text-l text-blue-500">{state}</span>
@@ -54,8 +61,12 @@ export default function () {
             </li>
           ))}
         </ul>
-      </nav>
+      </NavSection>
       <div>{showMypageMainComp(nowState)}</div>
     </CommerceLayout>
   );
 }
+
+const NavSection = styled.nav`
+  background-color: ${(props) => props.color};
+`;
