8407cb937e43049afa16319eeee6bea8a2030069
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-10-26 10:45:08 +0900

    Fix: 메인 이벤트 페이지 목록 표시 수정

--- components/event/Event.js
+++ components/event/Event.js
@@ -2,95 +2,125 @@ import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
 import { EVENT_TYPE } from '@utils/constants/types';
 import { getDateTime, getState } from '@utils/functions';
+import {
+  ThemeGray4,
+  ThemeGray1,
+  ThemeWhite,
+  ThemeBlack,
+  MainBlue,
+} from '@utils/constants/themeColor';
 
 export default function Event({ event }) {
   const router = useRouter();
-  const onClick = (id) => {
+  const goToEventDetail = (id) => {
     router.push({
       pathname: `/event/${id}`,
     });
   };
 
   return (
-    <>
-      <EventDiv>
-        <a className="cursor-pointer" onClick={() => onClick(event.id)}>
-          <div>
-            <EventImage
-              src={event.eventImage.eventBannerImg}
-              alt={event.descript}
-            />
-          </div>
-          <Content>
-            <Left>{event.storeName}</Left>
-            <Right>
-              <Tags>{getState(EVENT_TYPE, event.type)}</Tags>
-            </Right>
+    <EventSection>
+      <a className="cursor-pointer" onClick={() => goToEventDetail(event.id)}>
+        <ImgSection className="img-section">
+          <EventImage
+            src={event.eventImage.eventBannerImg}
+            alt={event.descript}
+          />
+        </ImgSection>
+        <ContentSection className="content-section">
+          <TitleSection className="title-section">{event.title}</TitleSection>
+          <TypeSection className="type-section">
+            <StoreNameSection>{event.storeName}</StoreNameSection>
+            <Tags>{getState(EVENT_TYPE, event.type)}</Tags>
+          </TypeSection>
+          <TimeSection className="time-section">
+            {getDateTime(event.startAt)}
             <br />
-            <br />
-            <Title>{event.title}</Title>
-            <Time>
-              {getDateTime(event.startAt)} ~<br /> {getDateTime(event.endAt)}
-            </Time>
-          </Content>
-        </a>
-      </EventDiv>
-    </>
+            {' ~ '}
+            {getDateTime(event.endAt)}
+          </TimeSection>
+        </ContentSection>
+      </a>
+    </EventSection>
   );
 }
 
-const EventDiv = styled.div`
-  display: inline-block;
-  height: 23rem;
-  width: 28rem;
-  margin: 1rem;
-  padding: 1rem;
-  border-radius: 1rem;
-  border: 1px solid #d6d6d6;
+const EventSection = styled.li`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  min-width: 300px;
+  margin: 10px;
+  border-radius: 10px;
+  border: 1px solid ${ThemeGray4};
+  overflow: hidden;
+
+  @media (min-width: 1024px) {
+    width: 47%;
+  }
+
+  @media (max-width: 1024px) {
+    width: 100%;
+  }
 `;
 
-const Content = styled.div`
-  padding: 1rem;
-`;
-
-const Title = styled.div`
-  font-size: 1.5rem;
-  font-weight: bold;
-`;
-
-const Left = styled.div`
-  font-weight: bold;
-  float: left;
-  margin: 0 auto;
-  font-size: 1.5rem;
-  color: #999;
-`;
-
-const Right = styled.div`
-  font-weight: bold;
-  float: right;
-  margin: 0 auto;
-  font-size: 1.5rem;
-  color: #999;
-`;
-
-const Time = styled.div`
-  margin-top: 0.5rem;
-  font-size: 1rem;
+const ImgSection = styled.div`
+  overflow: hidden;
+  border-radius: 6px;
+  width: 100%;
+  padding: 10px;
+  text-align: center;
 `;
 
 const EventImage = styled.img`
-  width: 25rem;
-  height: 10rem;
+  overflow: hidden;
+  object-fit: cover;
+  object-position: center;
+  aspect-ratio: 500 / 200;
+  border-radius: 6px;
   margin: auto;
-  margin-bottom: 1rem;
+  box-shadow: 0 5px 18px -7px ${ThemeGray1};
+`;
+
+const ContentSection = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  padding: 1rem;
+`;
+
+const TitleSection = styled.div`
+  margin-bottom: 10px;
+  font-size: x-large;
+  font-weight: 700;
+  text-align: left;
+  max-width: 100%;
+  min-width: 200px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const StoreNameSection = styled.div`
+  font-weight: bold;
+  font-size: 1.5rem;
+  color: ${MainBlue};
+`;
+
+const TypeSection = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+`;
+
+const TimeSection = styled.div`
+  padding-top: 10px;
+  text-align: left;
 `;
 
 const Tags = styled.span`
-  background-color: black;
-  color: #fff;
+  background-color: ${ThemeBlack};
+  color: ${ThemeWhite};
   font-size: 1rem;
-  margin: 0 0.5rem;
+  margin-left: auto;
   padding: 0.5rem 1rem;
   border-radius: 3rem;
 `;
