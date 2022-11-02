071f8f075cdd1c2d1545b57d4d9b5be64f4c8149
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-11-02 21:47:04 +0900

    Fix: 404 페이지 문구 수정

--- pages/404.js
+++ pages/404.js
@@ -8,23 +8,27 @@ export default function NotFoundPage() {
 
   return (
     <CommerceLayout>
-      <div class="bg-white py-6 sm:py-8 lg:py-12">
+      <div class="py-6 sm:py-8 lg:py-12">
         <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
           <div class="flex flex-col items-center">
             <a
               href="/"
-              class="inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5 mb-8"
+              class="inline-flex items-center text-2xl md:text-3xl font-bold gap-2.5 mb-8"
               aria-label="logo"
             >
               <EmptyImage src="/parabole.svg" />
             </a>
 
-            <h1 class="text-gray-800 text-2xl md:text-3xl font-bold text-center mb-2">
+            <p class="text-2xl md:text-3xl font-bold text-center mb-4">
               Page not found
-            </h1>
+            </p>
 
-            <p class="max-w-screen-md text-gray-500 md:text-lg text-center mb-12">
-              The page you’re looking for doesn’t exist.
+            <p class="max-w-screen-md md:text-lg text-center">
+              페이지가 존재하지 않습니다.
+            </p>
+
+            <p class="max-w-screen-md md:text-lg text-center mb-12">
+              입력하신 주소가 정확한지 다시 한번 확인해 주세요.
             </p>
 
             <Blue
