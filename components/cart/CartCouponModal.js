65b556b57d6d7cf16e4f5d0b0976c93bee869f16
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-11-03 14:24:07 +0900

    Fix: 장바구니 쿠폰 모달 반응형 적용

--- components/cart/CartCouponModal.js
+++ components/cart/CartCouponModal.js
@@ -204,24 +204,29 @@ function CouponTableRow({
 }
 
 const ModalContainer = styled.div`
-  /* 모달창 크기 */
-  width: 800px;
-
   /* 최상단 위치 */
   z-index: 999;
 
   /* 중앙 배치 */
   /* top, bottom, left, right 는 브라우저 기준으로 작동한다. */
   /* translate는 본인의 크기 기준으로 작동한다. */
-  position: absolute;
+  position: fixed;
   top: 50%;
   left: 50%;
-  transform: translate(-30%, -50%);
+  transform: translate(-50%, -50%);
 
   /* 모달창 디자인 */
   background-color: ${ThemeGray5};
   border: 1px solid black;
   border-radius: 8px;
+
+  /* 모달창 크기 */
+  @media (max-width: 1024px) {
+    width: 90%;
+  }
+  @media (min-width: 1024px) {
+    width: 800px;
+  }
 `;
 
 const TopSection = styled.div`
