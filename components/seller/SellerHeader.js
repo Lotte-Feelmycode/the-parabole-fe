0ad6dbc554fb0877c0b340cee88692407d6af3fa
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-11-03 12:01:29 +0900

    Fix: SellerHeader 에러 수정 & main 문구 수정

--- components/seller/SellerHeader.js
+++ components/seller/SellerHeader.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function SellerHeader() {
   const linkSellerHome = '/seller/main';
