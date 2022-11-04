a7cb4bf59050a7b7de1d364d4e26ff106d21b91f
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2022-11-04 10:59:40 +0900

    Hotfix: import useEffect in SellerHeader

--- components/seller/SellerHeader.js
+++ components/seller/SellerHeader.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function SellerHeader() {
   const linkSellerHome = '/seller/main';
