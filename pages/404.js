19c0003b2416c9cc7236c6085d2ebb066164adfd
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-11-08 21:18:09 +0900

    Fix: className을 class로 작성했던 오류 수정

--- pages/404.js
+++ pages/404.js
@@ -8,26 +8,26 @@ export default function NotFoundPage() {
 
   return (
     <CommerceLayout>
-      <div class="py-6 sm:py-8 lg:py-12">
-        <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-          <div class="flex flex-col items-center">
+      <div className="py-6 sm:py-8 lg:py-12">
+        <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
+          <div className="flex flex-col items-center">
             <a
               href="/"
-              class="inline-flex items-center text-2xl md:text-3xl font-bold gap-2.5 mb-8"
+              className="inline-flex items-center text-2xl md:text-3xl font-bold gap-2.5 mb-8"
               aria-label="logo"
             >
               <EmptyImage src="/parabole.svg" />
             </a>
 
-            <p class="text-2xl md:text-3xl font-bold text-center mb-4">
+            <p className="text-2xl md:text-3xl font-bold text-center mb-4">
               Page not found
             </p>
 
-            <p class="max-w-screen-md md:text-lg text-center">
+            <p className="max-w-screen-md md:text-lg text-center">
               페이지가 존재하지 않습니다.
             </p>
 
-            <p class="max-w-screen-md md:text-lg text-center mb-12">
+            <p className="max-w-screen-md md:text-lg text-center mb-12">
               입력하신 주소가 정확한지 다시 한번 확인해 주세요.
             </p>
 
