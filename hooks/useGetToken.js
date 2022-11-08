1db6ba4a160905bae61e9b0eeab63c0fbec4fb72
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2022-11-08 11:59:12 +0900

    Feat: 소셜 버튼 전부 제거

--- hooks/useGetToken.js
+++ hooks/useGetToken.js
@@ -1,22 +1,4 @@
-import { useRouter } from 'next/router';
-
 export const useGetToken = () => {
-  // const router = useRouter();
-  // if (typeof window === 'undefined') return;
-
-  // let token = localStorage.getItem('token');
-
-  // if (!token || token === 'undefined') {
-  //   localStorage.clear();
-  //   alert('로그인 후에 이용해주세요.');
-  //   router.push('/user/signin');
-  // } else if (obj) {
-  //   token = obj.value;
-  // }
-  // console.log('>>>>> Hook :: useGetToken', token);
-
-  // return token;
-
   let bearerToken = 'Bearer ';
   let accessToken;
   if (typeof window !== 'undefined' && typeof window !== undefined) {
