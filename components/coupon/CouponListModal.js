06354aee8f9aa62b5d7b8879f909d0a064779adb
IMHYEWON <hyewon1118@gmail.com>
2022-11-11 16:19:07 +0900

    Feat: 모달창 밖 클릭 이벤트

--- components/coupon/CouponListModal.js
+++ components/coupon/CouponListModal.js
@@ -33,7 +33,28 @@ export default function CouponListModal({
     });
   };
 
+  // 모달 외부 클릭시 끄기 처리
+  // Modal 창을 useRef로 취득
   const modalRef = useRef();
+
+  useEffect(() => {
+    const handler = () => {
+      // mousedown 이벤트가 발생한 영역이 모달창이 아닐 때, 모달창 제거 처리
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setModalState(false);
+      }
+    };
+
+    // 이벤트 핸들러 등록
+    document.addEventListener('mousedown', handler);
+    // document.addEventListener('touchstart', handler); // 모바일 대응
+
+    return () => {
+      // 이벤트 핸들러 해제
+      document.removeEventListener('mousedown', handler);
+      // document.removeEventListener('touchstart', handler); // 모바일 대응
+    };
+  });
 
   return (
     <BackgroundDIM>
