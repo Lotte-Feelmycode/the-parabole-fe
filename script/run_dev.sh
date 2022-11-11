5410b1cf15604585086f8e7d3cc10baa50a5913c
Eun chong <chd830@naver.com>
2022-11-11 19:11:14 +0900

    sh 파일 수정

--- script/run_dev.sh
+++ script/run_dev.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-ssh ec2-user@3.39.167.221 <<EOF	
+cd ~/the-parabole-fe
 
-    cd ~/the-parabole-fe
- 
-    git pull -f origin dev
+git pull -f origin dev
 
-		cd submodule
-		git fetch 
-		git merge origin main
+cd submodule
+git fetch 
+git merge origin main
 
-		cp .env ../.env
+cp .env ../.env
 
-    npm install
+npm install
 
-    npm install -g pm2
+npm install -g pm2
 
-		npx pm2 start npm --start
-    exit
-EOF
+pm2 reload all
