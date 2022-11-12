364e33e5656bc37a9fe685656df4b86be29fd0d0
Eun chong <chd830@naver.com>
2022-11-12 19:05:49 +0900

    Update [id].js

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -34,7 +34,7 @@ export default function ProductDetail() {
     if (typeof window !== 'undefined' && typeof window !== undefined) {
       if (localStorage.getItem('userId') === null) {
         alert('로그인 해주세요.');
-        router.push('/signin');
+        router.push(LINKS.SIGNIN);
       }
     }
     setHeaders(useGetToken());
