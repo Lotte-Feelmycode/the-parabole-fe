d0125724ba70d261f2441c0fd2e32125566964e7
IMHYEWON <hyewon1118@gmail.com>
2022-11-14 03:21:48 +0900

    Feat: 알림 팝업 컴포넌트

---
+++ components/common/ToastPopup.js
@@ -0,0 +1,19 @@
+import { ToastContainer, toast } from 'react-toastify';
+
+export default function Toast() {
+  return (
+    <ToastContainer
+      position="top-center"
+      autoClose={500}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+
+  )
+}
