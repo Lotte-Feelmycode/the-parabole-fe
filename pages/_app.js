c20587270ed2df67f194099b24da97288842c177
IMHYEWON <hyewon1118@gmail.com>
2022-11-14 03:21:06 +0900

    Feat: 알러트, 컨펌 팝업 라이브러리 설치

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import styled from '@emotion/styled';
 import '@styles/globals.scss';
 import { Html } from 'next/document';
 import 'pages/app.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 function MyApp({ Component, pageProps }) {
   return (
