17a1855f4b93801835aa573078541bd7c422c55e
IMHYEWON <hyewon1118@gmail.com>
2022-11-14 04:27:41 +0900

    Design: 반응형 수정

--- components/event/EvenInfo.js
+++ components/event/EvenInfo.js
@@ -6,34 +6,6 @@ import styled from '@emotion/styled';
 export default function EventInfo({ eventInfo, eventImage }) {
   return (
     <>
-      <div
-        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
-        aria-hidden="true"
-      >
-        <svg
-          width="100%"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs>
-            <linearGradient
-              x1="50%"
-              y1="0%"
-              x2="50%"
-              y2="100%"
-              id="illustration-01"
-            >
-              <stop stopColor="#FFF" offset="0%" />
-              <stop stopColor="#EAEAEA" offset="77.402%" />
-              <stop stopColor="#DFDFDF" offset="100%" />
-            </linearGradient>
-          </defs>
-          <g fill="url(#illustration-01)" fillRule="evenodd">
-            <circle cx="1232" cy="128" r="128" />
-            <circle cx="155" cy="443" r="64" />
-            <circle cx="422" cy="443" r="12" />
-          </g>
-        </svg>
-      </div>
       <section className="relative">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
