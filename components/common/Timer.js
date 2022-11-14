c3b195f86b2f3b5a4f3f4fabaca03b33bd30416f
IMHYEWON <hyewon1118@gmail.com>
2022-11-14 15:34:54 +0900

    Feat: 이벤트 남은 시간 알려주는 타이머 컴포넌트

--- components/common/Timer.js
+++ components/common/Timer.js
@@ -1,37 +1,40 @@
-import { useState, useEffect } from "react";
-import { getNewDate } from "@utils/functions";
+import { useState, useEffect } from 'react';
+import { getNewDate } from '@utils/functions';
 const Timer = ({ endAt }) => {
-  var endAt = new Date(endAt);
-  var now = new Date();
+  let endDate = new Date(endAt);
+  let now = new Date();
 
-  var diffTMins = (endAt.getTime() - now.getTime()) / (1000*60);
+  if (endDate.getTime() - now.getTime() < 0) {
+    return <div>0:00</div>;
+  } else {
+    let diffTMins = (endDate.getTime() - now.getTime()) / (1000 * 60);
 
-  const [minutes, setMinutes] = useState(parseInt(diffTMins));
-  const [seconds, setSeconds] = useState(parseInt(0));
+    const [minutes, setMinutes] = useState(parseInt(diffTMins));
+    const [seconds, setSeconds] = useState(parseInt(0));
 
-  console.log(parseInt(diffTMins));
-  useEffect(() => {
-    const countdown = setInterval(() => {
-      if (parseInt(seconds) > 0) {
-        setSeconds(parseInt(seconds) - 1);
-      }
-      if (parseInt(seconds) === 0) {
-        if (parseInt(minutes) === 0) {
-          clearInterval(countdown);
-        } else {
-          setMinutes(parseInt(minutes) - 1);
-          setSeconds(59);
+    useEffect(() => {
+      const countdown = setInterval(() => {
+        if (parseInt(seconds) > 0) {
+          setSeconds(parseInt(seconds) - 1);
         }
-      }
-    }, 1000);
-    return () => clearInterval(countdown);
-  }, [minutes, seconds]);
+        if (parseInt(seconds) === 0) {
+          if (parseInt(minutes) === 0) {
+            clearInterval(countdown);
+          } else {
+            setMinutes(parseInt(minutes) - 1);
+            setSeconds(59);
+          }
+        }
+      }, 1000);
+      return () => clearInterval(countdown);
+    }, [minutes, seconds]);
 
-  return (
-    <div>
-      {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-    </div>
-  );
+    return (
+      <div>
+        {minutes}:{seconds < 10 ? `0${seconds}` : seconds} 남음
+      </div>
+    );
+  }
 };
 
 export default Timer;
