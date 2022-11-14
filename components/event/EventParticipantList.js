9392d316f1ec33f44c4de7c4f4736c6ac4237423
IMHYEWON <hyewon1118@gmail.com>
2022-11-14 21:17:12 +0900

    [WIP] feat: event 정보

--- components/event/EventParticipantList.js
+++ components/event/EventParticipantList.js
@@ -1,45 +1,62 @@
 import { GET_DATA } from '@apis/defaultApi';
 import { useEffect, useState } from 'react';
 import EventParticipant from './EventParticipant';
-
+import { dto } from '@pages/seller/event/DummyData';
+import { SortButton } from '@components/input/SortButton';
 export default function EventParticipantList({ eventId }) {
   const [participantList, setParticipantList] = useState([]);
 
   useEffect(() => {
-    if (eventId) {
-      GET_DATA(`/event/seller/participant/${eventId}`).then((res) => {
-        setParticipantList(res);
-      });
-    }
-  }, [eventId]);
+    console.log(dto);
+    setParticipantList(dto.data);
+    // if (eventId) {
+    //   GET_DATA(`/event/seller/participant/${eventId}`).then((res) => {
+    //     setParticipantList(res);
+    //   });
+    // }
+  }, []);
 
   return (
     <>
       {participantList && participantList.length > 0 ? (
-        <table className="w-2/3 text-m text-center">
-          <thead className="text-m uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+        <table className="text-center w-4/5 text-xl text-left text-gray-500 dark:text-gray-400">
+          <caption className="py-5 text-left bg-white dark:text-white dark:bg-gray-800">
+            <p className="text-xl font-semibold text-gray-900">
+              이벤트 응모 고객 목록
+            </p>
+            <p className="mt-1 text-m text-gray-700">
+              이벤트에 응모한 고객 목록을 확인할 수 있습니다.
+            </p>
+          </caption>
+          <thead className="text-base text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
             <tr className="h-12">
-              <td scope="col" className="py-1 px-4">
+              <th scope="col" className="py-3 px-2 w-20">
+                번호
+              </th>
+              <th scope="col" className="py-3 px-6 w-60">
+                <div className="flex items-center justify-center">
+                  이름
+                  {/* <SortButton /> */}
+                </div>
+              </th>
+              <th scope="col" className="py-3 px-6 w-60">
                 이메일
-              </td>
-              <td scope="col" className="py-1 px-4">
-                이름
-              </td>
-              <td scope="col" className="py-1 px-2">
-                선택 경품
-              </td>
-              <td scope="col" className="py-1 px-2">
+              </th>
+              <th scope="col" className="py-3 px-6 w-100">
+                <div className="flex items-center justify-center">
+                  선택 경품
+                  {/* <SortButton /> */}
+                </div>
+              </th>
+              <td scope="col" className="py-3 px-2 w-60">
                 이벤트 참여 일자
               </td>
             </tr>
           </thead>
           <tbody>
             {participantList &&
-              participantList.map((participant) => (
-                <EventParticipant
-                  key={participant.id}
-                  participant={participant}
-                />
+              participantList.map((participant, index) => (
+                <EventParticipant idx={index} participant={participant} />
               ))}
           </tbody>
         </table>
--- pages/seller/event/[id].js
+++ pages/seller/event/[id].js
@@ -10,6 +10,7 @@ import { getDateTime, getState } from '@utils/functions';
 import { PRIZE_TYPE, EVENT_TYPE } from '@utils/constants/types';
 import EventParticipant from '@components/event/EventParticipantList';
 import { useGetToken } from '@hooks/useGetToken';
+import * as ICON from '@utils/constants/icons';
 
 export default function EventDetail() {
   const router = useRouter();
@@ -21,15 +22,15 @@ export default function EventDetail() {
   const [headers, setHeaders] = useState();
 
   useEffect(() => {
-    if (typeof window !== 'undefined' && typeof window !== undefined) {
-      if (localStorage.getItem('userId') === null) {
-        alert('로그인 해주세요.');
-        router.push('/signin');
-      } else if (localStorage.getItem('role') === 'ROLE_USER') {
-        alert('판매자 페이지입니다.');
-        router.push('/');
-      }
-    }
+    // if (typeof window !== 'undefined' && typeof window !== undefined) {
+    //   if (localStorage.getItem('userId') === null) {
+    //     alert('로그인 해주세요.');
+    //     router.push('/signin');
+    //   } else if (localStorage.getItem('role') === 'ROLE_USER') {
+    //     alert('판매자 페이지입니다.');
+    //     router.push('/');
+    //   }
+    // }
     setHeaders(useGetToken());
   }, []);
 
@@ -62,34 +63,38 @@ export default function EventDetail() {
     <SellerLayout>
       <SiteHead title={'Seller Office'} />
 
-      <section className="flex min-h-screen flex-col text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <Heading title="이벤트 상세" type="h1" />
+      <section className="flex min-h-screen flex-col text-gray-900 body-font">
+        <div className="container px-5 py-24 mx-auto items-center">
+          <a className="flex flex-row">
+            <span className="w-9 h-9 mr-2 rounded-full inline-flex justify-center">
+              <img src={ICON.ICON_LIST}></img>
+            </span>
+            <span className="text-4xl font-bold mb-2">이벤트 상세</span>
+          </a>
           <Divider />
-          <Heading title={event.title} type="h2" />
-          <br />
-          <Heading title="이벤트 유형" type="h3" />
-          <span>
-            {getState(EVENT_TYPE, event.type)}
-            &nbsp;이벤트
-          </span>
-          <br />
-          <br />
-
-          <Heading title="이벤트 일시" type="h3" />
           <div className="mb-8">
-            <span>
+            <span className="text-2xl font-bold mb-2">{event.title}</span>
+          </div>
+          <div className="flex flex-col mb-8">
+            <span className="text-xl font-bold">이벤트 유형</span>
+            <span className="text-l ml-2">
+              {getState(EVENT_TYPE, event.type)} 이벤트
+            </span>
+          </div>
+
+          <div className="flex flex-col mb-8">
+            <span className="text-xl font-bold">이벤트 일시</span>
+            <span className="ml-2">
               이벤트 시작일시 : {event.startAt && getDateTime(event.startAt)}
             </span>
-            <br />
-            <span>
+            <span className="ml-2">
               이벤트 종료일시 : {event.endAt && getDateTime(event.endAt)}
             </span>
           </div>
 
-          <div className="mb-8">
-            <Heading title="경품" type="h3" />
-            <table className="w-2/3 text-m text-center px-4 pb-8">
+          <div className="flex flex-col mb-8">
+            <span className="text-xl font-bold mb-2">경품</span>
+            <table className="ml-2 w-2/3 text-m text-center px-4 pb-8">
               <thead className="text-base text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                 <tr className="h-14">
                   <th scope="col" className="py-3 px-4 w-20">
@@ -135,7 +140,6 @@ export default function EventDetail() {
           </div>
 
           <div className="mb-8">
-            <Heading title="참여자" type="h3" />
             <EventParticipant eventId={eventId}></EventParticipant>
           </div>
           <Heading title="이벤트 이미지" type="h3" />
