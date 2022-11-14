36d5f7fd8cf89a8bedfff149edf717a8f13c1a37
IMHYEWON <hyewon1118@gmail.com>
2022-11-14 15:35:16 +0900

    Fix: 이벤트 정보 화면 수정

--- components/event/EvenInfo.js
+++ components/event/EvenInfo.js
@@ -1,10 +1,30 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useRouter } from 'next/router';
 import * as btn from '@components/input/Button';
 import { getDateTime } from '@utils/functions';
 import styled from '@emotion/styled';
 import Timer from '@components/common/Timer';
+import { useState, useEffect } from 'react';
+import { GET_DATA } from '@apis/defaultApi';
 
 export default function EventInfo({ eventInfo, eventImage }) {
+  const router = useRouter();
+  const [storeInfo, setStoreInfo] = useState();
+
+  useEffect(() => {
+    if (eventInfo) {
+      GET_DATA(`/seller`, { sellerId: eventInfo.sellerId }).then((res) => {
+        if (res) {
+          setStoreInfo(res);
+        }
+      });
+    }
+  }, [router.query]);
+  function goToStore() {
+    if (storeInfo && storeInfo.sellerId) {
+      router.push({ pathname: `/store/${storeInfo.sellerId}` });
+    }
+  }
+
   return (
     <>
       <section className="relative">
@@ -14,32 +34,42 @@ export default function EventInfo({ eventInfo, eventImage }) {
               <H1
                 className="break-all text-4xl md:text-5xl lg:text-6xl leading-tighter tracking-tighter"
                 data-aos="zoom-y-out"
-                >
+              >
                 {eventInfo.descript}
               </H1>
               <H1
                 className="break-all text-4xl md:text-7xl lg:text-8xl font-bold leading-tighter tracking-tighter
                   bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 whitespace-pre-line"
                 data-aos="zoom-y-out"
-                >
+              >
                 {eventInfo.title}
               </H1>
-              <div className="max-w-3xl mx-auto">
-                <p
-                  className="text-xl md:text-2xl text-gray-600 mb-8"
-                  data-aos="zoom-y-out"
-                  data-aos-delay="150"
-                >
-                  {eventInfo.storeName}에서 {getDateTime(eventInfo.startAt)} ~{' '}
-                  {getDateTime(eventInfo.endAt)}동안 진행하는 이벤트!
-                </p>
+              <div className="max-w-3xl mx-auto mt-5">
+                <div>
+                  <div className="flex justify-center item-center flex-row">
+                    <p className="text-3xl md:text-4xl font-bold text-blue-600 mr-2">
+                      "{storeInfo && storeInfo.storeName}"
+                    </p>
+                    <p className="text-xl md:text-2xl text-gray-600 mb-8">
+                      에서
+                    </p>
+                  </div>
+                  <div className="flex justify-center item-center flex-col">
+                    <p className="text-xl md:text-2xl text-gray-600">
+                      {getDateTime(eventInfo.startAt)}부터
+                    </p>
+                    <p className="text-xl md:text-2xl text-gray-600 mb-8">
+                      {getDateTime(eventInfo.endAt)}까지 진행하는 이벤트!
+                    </p>
+                  </div>
+                </div>
                 <p
                   className="text-3xl md:text-4xl text-gray-600 mb-8"
                   data-aos="zoom-y-out"
                   data-aos-delay="150"
                 >
                   {/* TODO : 이벤트 종료일시 - 현재 */}
-                  <Timer endAt={eventInfo.endAt}/>
+                  <Timer endAt={eventInfo.endAt} />
                 </p>
                 <div
                   className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
@@ -47,7 +77,10 @@ export default function EventInfo({ eventInfo, eventImage }) {
                   data-aos-delay="300"
                 >
                   <div className="animate-pulse">
-                    <btn.Blue buttonText={'상품 더 보러가기'} />
+                    <btn.Blue
+                      buttonText={'상품 더 보러가기'}
+                      onClickFunc={goToStore}
+                    />
                   </div>
                 </div>
               </div>
--- pages/event/[id].js
+++ pages/event/[id].js
@@ -2,23 +2,42 @@ import { useState, useEffect, useMemo } from 'react';
 import { useRouter } from 'next/router';
 import styled from '@emotion/styled';
 import { GET_DATA, POST_DATA } from '@apis/defaultApi';
-import { getDateTime } from '@utils/functions';
-import EventPrize from '@components/event/eventPrize';
 import CommerceLayout from '@components/common/CommerceLayout';
-import Timer from '@components/common/Timer';
 import EventInfo from '@components/event/EvenInfo';
 import EventPrizeContainer from '@components/event/EventPrizeContainer';
+import { useGetToken } from '@hooks/useGetToken';
 
 export default function EventDetail() {
-  //TODO userId 객체 받아오기
-  const userId = 3;
-
   const [eventInfo, setEventInfo] = useState({});
   const [eventImage, setEventImage] = useState({});
+  const [storeInfo, setStoreInfo] = useState();
   const router = useRouter();
   const [eventId, setEventId] = useState(router.query.id);
   const [eventPrizes, setEventPrizes] = useState([]);
   const [applyStatus, setApplyStatus] = useState('');
+
+  const [headers, setHeaders] = useState();
+  useEffect(() => {
+    let getHeaders = useGetToken();
+    setHeaders(getHeaders);
+
+    if (headers) {
+      //TODO : 백엔드 수정 후 삭제될 파라미터
+      const userId = localStorage.getItem('userId');
+      POST_DATA(
+        '/event/participant/check',
+        {
+          userId,
+          eventId,
+        },
+        headers,
+      ).then((res) => {
+        if (!res) {
+          setApplyStatus('disabled');
+        }
+      });
+    }
+  }, []);
 
   useEffect(() => {
     const eventId = router.query.id;
@@ -30,6 +49,7 @@ export default function EventDetail() {
         setEventImage(res.eventImage);
         setEventPrizes(res.eventPrizes);
 
+        console.log('진행상태', res.status);
         if (res.status !== 1) {
           setApplyStatus('disabled');
         }
@@ -37,32 +57,34 @@ export default function EventDetail() {
     });
   }, [router.query]);
 
-  useEffect(() => {
-    POST_DATA('/event/participant/check', {
-      userId,
-      eventId,
-    }).then((res) => {
-      if (!res) {
-        setApplyStatus('disabled');
-      }
-    });
-  }, []);
-
   return (
     <CommerceLayout>
-        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none" aria-hidden="true">
-          <svg className="w-screen" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
-                <stop stopColor="#FFF" offset="0%" />
-                <stop stopColor="#EAEAEA" offset="77.402%" />
-                <stop stopColor="#DFDFDF" offset="100%" />
-              </linearGradient>
-            </defs>
-            <g fill="url(#illustration-01)" fillRule="evenodd">
-              <circle cx="1232" cy="128" r="128" />
-              <circle cx="155" cy="443" r="64" />
-            </g>
+      <div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        <svg
+          className="w-screen"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
         </svg>
       </div>
       <main className="flex-grow">
