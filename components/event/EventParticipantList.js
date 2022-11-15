a218ea9481e40fcb377e97c38f66715e3a3a51ef
IMHYEWON <hyewon1118@gmail.com>
2022-11-15 10:19:28 +0900

    Feat: 이벤트 참여자내역

--- components/event/EventParticipantList.js
+++ components/event/EventParticipantList.js
@@ -2,19 +2,109 @@ import { GET_DATA } from '@apis/defaultApi';
 import { useEffect, useState } from 'react';
 import EventParticipant from './EventParticipant';
 import { dto } from '@pages/seller/event/DummyData';
-import { SortButton } from '@components/input/SortButton';
+import SortButton from '@components/input/SortButton';
+import { isEmpty } from './../../utils/functions';
 export default function EventParticipantList({ eventId }) {
   const [participantList, setParticipantList] = useState([]);
 
+  const [mailSortDesc, setMailSortDesc] = useState(false);
+  const [nameSortDesc, setNameSortDesc] = useState(false);
+  const [dateSortDesc, setDateSortDesc] = useState(true);
+
   useEffect(() => {
-    console.log(dto);
-    setParticipantList(dto.data);
-    // if (eventId) {
-    //   GET_DATA(`/event/seller/participant/${eventId}`).then((res) => {
-    //     setParticipantList(res);
-    //   });
-    // }
+    if (eventId) {
+      GET_DATA(`/event/seller/participant/${eventId}`).then((res) => {
+        setParticipantList(res);
+      });
+    }
   }, []);
+
+  function sortListByMail(e) {
+    e.preventDefault();
+
+    let copyArray = [...participantList];
+
+    if (mailSortDesc) {
+      copyArray.sort(function (a, b) {
+        if (!(isEmpty(a) || isEmpty(b)))
+          // 내림차순
+          return a.userEmail > b.userEmail
+            ? -1
+            : a.userEmail < b.userEmail
+            ? 1
+            : 0;
+      });
+      setMailSortDesc(false);
+    } else {
+      copyArray.sort(function (a, b) {
+        // 오름차순
+        if (!(isEmpty(a) || isEmpty(b)))
+          return a.userEmail < b.userEmail
+            ? -1
+            : a.userEmail > b.userEmail
+            ? 1
+            : 0;
+      });
+      setMailSortDesc(true);
+    }
+
+    setParticipantList(copyArray);
+  }
+
+  function sortListByName(e) {
+    e.preventDefault();
+    let copyArray = [...participantList];
+
+    if (dateSortDesc) {
+      copyArray.sort(function (a, b) {
+        if (!(isEmpty(a) || isEmpty(b)))
+          // 내림차순
+          return a.userName > b.userName ? -1 : a.userName < b.userName ? 1 : 0;
+      });
+      setDateSortDesc(false);
+    } else {
+      copyArray.sort(function (a, b) {
+        // 오름차순
+        if (!(isEmpty(a) || isEmpty(b)))
+          return a.userName < b.userName ? -1 : a.userName > b.userName ? 1 : 0;
+      });
+      setDateSortDesc(true);
+    }
+
+    setParticipantList(copyArray);
+  }
+
+  function sortListByDate(e) {
+    e.preventDefault();
+
+    let copyArray = [...participantList];
+
+    if (nameSortDesc) {
+      copyArray.sort(function (a, b) {
+        if (!(isEmpty(a) || isEmpty(b)))
+          // 내림차순
+          return a.eventTimeStartAt > b.eventTimeStartAt
+            ? -1
+            : a.eventTimeStartAt < b.eventTimeStartAt
+            ? 1
+            : 0;
+      });
+      setNameSortDesc(false);
+    } else {
+      copyArray.sort(function (a, b) {
+        if (!(isEmpty(a) || isEmpty(b)))
+          // 오름차순
+          return a.eventTimeStartAt < b.eventTimeStartAt
+            ? -1
+            : a.eventTimeStartAt > b.eventTimeStartAt
+            ? 1
+            : 0;
+      });
+      setNameSortDesc(true);
+    }
+
+    setParticipantList(copyArray);
+  }
 
   return (
     <>
@@ -28,20 +118,31 @@ export default function EventParticipantList({ eventId }) {
               <th scope="col" className="py-3 px-6 w-60">
                 <div className="flex items-center justify-center">
                   이름
-                  {/* <SortButton /> */}
+                  <SortButton
+                    onClickFunc={(e) => sortListByName(e)}
+                  ></SortButton>
                 </div>
               </th>
               <th scope="col" className="py-3 px-6 w-60">
-                이메일
+                <div className="flex items-center justify-center">
+                  이메일
+                  <SortButton
+                    onClickFunc={(e) => sortListByMail(e)}
+                  ></SortButton>
+                </div>
               </th>
               <th scope="col" className="py-3 px-6 w-100">
                 <div className="flex items-center justify-center">
                   선택 경품
-                  {/* <SortButton /> */}
                 </div>
               </th>
               <td scope="col" className="py-3 px-2 w-60">
-                이벤트 참여 일자
+                <div className="flex items-center justify-center">
+                  응모일시
+                  <SortButton
+                    onClickFunc={(e) => sortListByDate(e)}
+                  ></SortButton>
+                </div>
               </td>
             </tr>
           </thead>
