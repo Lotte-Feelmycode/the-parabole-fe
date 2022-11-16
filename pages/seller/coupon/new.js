66ef71928453d11c1f6a9d9974e45bcca09e2ee0
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2022-11-16 16:07:05 +0900

    Fix: placeHolder 문구 수정

--- pages/seller/coupon/new.js
+++ pages/seller/coupon/new.js
@@ -256,7 +256,7 @@ export default function CouponCreate() {
                   className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
                   type="number"
                   name="cnt"
-                  placeHolder="쿠폰 발행 수량 ( 최대 100장 ) 을 입력해주세요."
+                  placeHolder="쿠폰 발행 수량을 입력해주세요. (최대 100)"
                   onChange={onChangeCnt}
                   required
                 />
