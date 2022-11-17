c639fc901d11d46a2c3d39732577c29d3c52d338
IMHYEWON <hyewon1118@gmail.com>
2022-11-17 11:39:45 +0900

    Fix: 원복

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -3,8 +3,10 @@ import { useRouter } from 'next/router';
 import styled from '@emotion/styled';
 import { GET_DATA, POST_DATA } from '@apis/defaultApi';
 import CommerceLayout from '@components/common/CommerceLayout';
-import SiteHead from '@components/common/SiteHead';
+import EventInfo from '@components/event/EvenInfo';
+import EventPrizeContainer from '@components/event/EventPrizeContainer';
 import { useGetToken } from '@hooks/useGetToken';
+import SiteHead from '@components/common/SiteHead';
 
 export default function EventDetail() {
   const [eventInfo, setEventInfo] = useState({});
@@ -53,43 +55,54 @@ export default function EventDetail() {
   }, [router.query]);
   return (
     <CommerceLayout>
-      <SiteHead title={eventInfo.title} description={eventInfo.descript} url={`https://theparabole.shop/${eventId}`}/>
-      <section className="flex min-h-screen flex-col text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div
-            className="event-detail-startAt"
-            style={{ fontSize: 'xx-large', padding: '10px' }}
-          >
-            {eventInfo && eventInfo.title}
-          </div>
-          <div
-            className="event-detail-startAt"
-            style={{ fontSize: 'large', padding: '10px' }}
-          >
-            {'응모 기간 : '}
-            {getDateTime(eventInfo.startAt)}
-            {' ~ '}
-            {getDateTime(eventInfo.endAt)}
-          </div>
-          <div className="event-detail-img" style={{ marginBottom: '100px' }}>
-            <img src={eventImage.eventDetailImg} style={{ width: '100%' }} />
-          </div>
-
-          <EventPrizeListSection className="prize-list">
-            {eventPrizes &&
-              eventPrizes.map((prize) => {
-                return (
-                  <EventPrize
-                    prize={prize}
-                    eventId={eventId}
-                    applyStatus={applyStatus}
-                    key={prize.eventPrizeId}
-                  />
-                );
-              })}
-          </EventPrizeListSection>
-        </div>
-      </section>
+      <SiteHead
+        title={eventInfo.title}
+        description={eventInfo.descript}
+        url={`https://theparabole.shop/${eventId}`}
+      />
+      <div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        <svg
+          className="w-screen"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div>
+      <main className="flex-grow">
+        <EventInfo
+          eventInfo={eventInfo}
+          eventImage={eventImage}
+          storeInfo={storeInfo}
+        />
+        <EventPrizeContainer
+          eventId={eventId}
+          eventPrizes={eventPrizes}
+          eventType={eventInfo.type}
+          eventStatus={eventInfo.status}
+          applyStatus={applyStatus}
+          headers={headers}
+        />
+      </main>
     </CommerceLayout>
   );
 }
