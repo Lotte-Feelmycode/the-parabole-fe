89632852f38cedf7c0a481d270dcec165461ea59
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-11-19 16:01:33 +0900

    Fix: prod 주소 변경

--- apis/api-config.js
+++ apis/api-config.js
@@ -4,7 +4,7 @@ let backendHost;
 const hostname = 'dev';
 
 if (hostname === 'prod') {
-  backendHost = 'http://api.theparabole.shop:8000';
+  backendHost = 'http://43.201.150.249:8000';
 } else if (hostname === 'dev') {
   backendHost = 'http://3.39.25.221:8000';
 } else {
